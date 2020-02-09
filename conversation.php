6f61a9ffd8f393ec1f59808be578317c58ff5739
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-02-09 09:41:49 +0100

    save 2

--- conversation.php
+++ conversation.php
@@ -3,8 +3,7 @@ session_start();
 
 $connexion = mysqli_connect("localhost", "root", "", "forum");
 date_default_timezone_set('europe/paris');
-
-
+include 'bar-nav.php';
 	    
  if (isset($_POST['submit']))
 {       
@@ -18,8 +17,27 @@ date_default_timezone_set('europe/paris');
         $iduser = $_SESSION["id"];
 		$requete2= "INSERT INTO conversations (title, description, user_id, date, topic_id, private) VALUES ('$titro', '$descrip', '$iduser', NOW(), '".$resultat[0][0]."', '$private')";
 		$query2= mysqli_query($connexion, $requete2);
+        header("location:conversation.php");
 
 }
+
+$requete1 = "SELECT conversations.id, conversations.title, conversations.description,conversations.date,utilisateurs.id, utilisateurs.login, topics.title, topics.id FROM topics INNER JOIN utilisateurs ON topics.user_id = utilisateurs.id INNER JOIN conversations ON topics.id = conversations.topic_id  ORDER BY conversations.id DESC ";
+$query1 = mysqli_query($connexion, $requete1);
+$resultat1 = mysqli_fetch_all($query1);
+
+
+foreach ($resultat1 as list($idco, $titreco, $descco, $dateco, $idu, $loginu, $titreto)) 
+{
+    echo "<p>".$titreto."</p>";
+    echo "<p>".$titreco."</p>";
+    echo "<p>".$descco."</p>";
+    echo "<p>".$dateco."</p>";
+    echo "<p>".$loginu."</p>";
+}
+
+ if (isset($_SESSION["login"])) 
+{
+
 ?>
 <form action="" method="post" class="ajout">
                 <label>Conversation</label>
@@ -29,5 +47,8 @@ date_default_timezone_set('europe/paris');
                 </select>
                 <input type="submit" value="Envoyer" name="submit">
             </form>
+<?php
+}
+?>
 
 
