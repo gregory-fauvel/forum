e16c088c0eb6b564ea632cebed89d77dcb98dd65
anthony-reboli <55872129+anthony-reboli@users.noreply.github.com>
2020-02-10 17:18:53 +0100

    en cour

---
+++ users.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+    <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="camping.css">
+    <title>Profil</title>
+</head>
+<body class="bodyc">
+
+  <?php
+  session_start();
+  // include("bar-nav.php");
+  if (isset($_SESSION['login']))
+  {
+    $id= $_GET['id'];
+    $connexion = mysqli_connect("localhost","root","","forum");
+
+    $requete = "SELECT * FROM utilisateurs WHERE id= $id";
+    $req = mysqli_query($connexion, $requete);
+    $data = mysqli_fetch_assoc($req);
+  
+  ?>
+    <section id="connexion">
+        
+      <div id="main" class="container">
+        <?php
+      $LoginS= $_SESSION['login']; 
+      ?>
+
+
+        <h1>Profil de <?php echo $data['name'] ?></h1>
+
+            
+
+           
+          <p>RanK: <?php echo $data['rank']?></p>
+          <p>Inscrit le: <?php echo $data['date']?></p>
+
+         </div>
+        
+   </section>
+  <?php
+  if ($_SESSION['login'] == 'admin'){
+    ?>
+    <label>Changer de grade</label>
+    <form method="post">
+    <select name="rank">
+      <option value="admin">Admin</option>;
+      <option value="modo">Moderateur</option>
+      <option value="Menbre">Membre</option>
+    </select>
+    <input type="submit" name="rankB">
+  </form>
+    <?php
+
+
+    if (isset($_POST['rankB']))
+    {
+      $rank =  $_POST['rank'];
+      $login = $_POST['login'];
+      
+
+      $requete2 = "UPDATE utilisateurs SET rank = '$rank' WHERE id = $id";
+      echo "$requete2";
+      $query2=mysqli_query($connexion,$requete2);
+      
+
+      // header("location:users.php");
+
+    }
+  }
+}
+  else 
+  {
+  ?>
+    <section id="notcon">
+      <p>Veuillez vous connecter pour accéder à votre page !</p>
+    </section>
+        <?php
+  }
+  
+// include("footer.php");
+?>
+ 
+  
+</body>
+</html>
+
+        
